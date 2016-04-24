e41664bd9f7ec4b68b250f7b3680f555fa4309d3
mcasl <manuel.castejon@gmail.com>
2016-04-25 00:14:26 +0200

    First working version of Container

--- container.py
+++ container.py
@@ -1,13 +1,23 @@
-class Container():
+class Container:
     """
     Interface for generic collection of items, ie. Conexions, Neurons, Layers, ...
     Currently though, it is nothing but a list wrapper.
     """
-    def __init__(self):
-        self.data = []
+
+    def __init__(self, value=None):
+        if value is None:
+            self.data = []
+        else:
+            self.data = value
+
+    def __getitem__(self, item):
+        result = self.data[item]
+        if isinstance(result, type(self.data)):
+            result = Container(result)
+        return result
 
     def __repr__(self):
-        return str(self.data)
+        return repr(self.data)
 
     def __len__(self):
         return len(self.data)
