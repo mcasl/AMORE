0c92036ec36e420ce354d5e25dc7e4890886422a
Manuel Castejon Limas <manuel.castejon@gmail.com>
2016-05-06 00:13:02 +0200

    Some activation function parameters changed to *args

--- test_amore.py
+++ test_amore.py
@@ -10,7 +10,7 @@ class TestConnection(unittest.TestCase):
     """ Tests for Connection class """
 
     @given(label=st.integers(), weight=st.floats())
-    def test_init_where_iterable_is_None(self, label, weight):
+    def test_init_where_iterable_is_none(self, label, weight):
         my_neuron = SimpleNeuron()
         my_neuron.label = label
         my_connection = Connection(my_neuron, weight)
